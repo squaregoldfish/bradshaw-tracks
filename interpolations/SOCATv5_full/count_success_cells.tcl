b2ae3c3ae6848c99a5d802f0a9a4f81377130a79
Steve Jones <steve@squaregoldfish.co.uk>
2018-02-05 08:51:10 +0100

    Curve fitting success counter

---
+++ interpolations/SOCATv5_full/count_success_cells.tcl
@@ -0,0 +1,31 @@
+#!/usr/bin/tclsh
+
+set dir "/Data/Scratch/science/bradshaw-tracks/interpolations/SOCATv5_full/interpolation_outputs/[lindex $argv 0]"
+
+set cellCount 0
+set cellTotal 10368.0
+
+set successCount 0
+
+for {set lon 1} {$lon <= 144} {incr lon} {
+    for {set lat 1} {$lat <= 72} {incr lat} {
+        incr cellCount
+        puts -nonewline "\r [format "%.2f" [expr $cellCount / $cellTotal * 100]]%    "
+        flush stdout
+
+        set logfile "${dir}/${lon}_${lat}.log"
+
+        if {[file exists $logfile]} {
+
+            set chan [open $logfile r]
+            set data [read $chan]
+            close $chan
+
+            if {[string first "SUCCESS" $data] > -1} {
+                incr successCount
+            }
+        }
+    }
+}    
+
+puts "\n$successCount"
