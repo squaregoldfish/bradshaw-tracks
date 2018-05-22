58fe80da6dd6f891dfb1706eced4c58b4dfd63db
Steve Jones <steve@squaregoldfish.co.uk>
2018-05-22 20:16:35 +0200

    Add Are tracks to SOCAT indices file

--- noresm/sampling/with_are_ship/add_are_track.tcl
+++ noresm/sampling/with_are_ship/add_are_track.tcl
@@ -1,13 +1,17 @@
 #!/usr/bin/tclsh
 
-# Extracts the ship code, day (as day index from 1985-01-01)
-# and lon/lat cell index to allow easy sampling of the NorESM model
+# This must be run after the without_ship run
 
-set IN_FILE "/Data/science/SOCAT/v5/SOCATv5.tsv"
-set OUT_FILE "/Data/Scratch/science/bradshaw-tracks/noresm/sampling/SOCATv5_indices.tsv"
+set START_YEAR 1985
+set END_YEAR 2016
 
-variable START_YEAR 1985
-variable END_YEAR 2016
+set track [lindex $argv 0]
+set areFile [lindex $argv 1]
+
+set IN_FILE "/Data/Scratch/science/bradshaw-tracks/noresm/sampling/${track}/SOCATv5_indices.tsv"
+set OUT_FILE "/Data/Scratch/science/bradshaw-tracks/noresm/sampling/${track}/SOCATv5_indices_with_are_ship.tsv"
+
+
 
 variable MONTH_STARTS [list -1 0 31 59 90 120 151 181 212 243 273 304 334]
 variable LEAP_MONTH_STARTS [list -1 0 31 60 91 121 152 182 213 244 274 305 335]
@@ -92,84 +96,74 @@ proc getLonCell {lon} {
     return [expr int($result)]
 }
 
-
-proc writeCell {outChan shipCode dateIndex lonCell latCell} {
-    if {[string length $shipCode] > 0} {
-        puts $outChan "$shipCode\t$dateIndex\t$lonCell\t$latCell"
-    }
+proc writeCell {outChan dateIndex lonCell latCell} {
+    puts $outChan "AREO\t$dateIndex\t$lonCell\t$latCell"
 }
 
-set outChan [open $OUT_FILE w]
-puts $outChan "Ship Code\tDay Index\tLon Index\tLat Index"
+# Duplicate the existing file, which doesn't have anything from our ship track in it
+file copy -force $IN_FILE $OUT_FILE
+
+# ...and open it for writing
+set outChan [open $OUT_FILE a]
+
+# Now load Are's track file
+set areChan [open $areFile]
+set areData [read $areChan]
+close $areChan
+
+# Skip the header
+set lines [lrange [split $areData "\n"] 1 end]
 
 
-set inChan [open $IN_FILE]
-
-set currentShipCode ""
 set currentDateIndex -1
 set currentLonCell -1
 set currentLatCell -1
 
-set lineCount 0
-
-while {[gets $inChan line] > 0} {
-
-    incr lineCount
-    if {[expr $lineCount % 1000] == 0} {
-        puts -nonewline "\033\[2K\r$lineCount"
-        flush stdout
-    }
+foreach line $lines {
 
     set fields [split $line "\t"]
-    
-    # Check that we're in the right time range
-    set year [lindex $fields 4]
-
-    if {$year >= $START_YEAR && $year <= $END_YEAR} {
+    if {[llength $fields] > 1} {
         set changed 0
 
-        set expoCode [lindex $fields 0]
-        set shipCode [string range $expoCode 0 [expr [string length $expoCode] - 9]]
+        set year [expr int([lindex $fields 1])]
+        set month [expr int([lindex $fields 2])]
+        set day [expr int([lindex $fields 3])]
 
-        if {[string compare $currentShipCode $shipCode] != 0} {
-            set changed 1
+        set lon [lindex $fields 6]
+        if {$lon < 0} {
+            set lon [expr 360 - abs($lon)]
         }
+        set lat [lindex $fields 7]
 
-        scan [lindex $fields 5] %d month
-        scan [lindex $fields 6] %d day
-
+        set dateIndex [getDateIndex $year $month $day]
 
         set dateIndex [getDateIndex $year $month $day]
         if {$dateIndex != $currentDateIndex} {
             set changed 1
         }
 
-        set lonCell [getLonCell [lindex $fields 10]]
+        set lonCell [getLonCell $lon]
         if {$lonCell != $currentLonCell} {
             set changed 1
         }
 
-        set latCell [getLatCell [lindex $fields 11]]
+        set latCell [getLatCell $lat]
         if {$latCell != $currentLatCell} {
             set changed 1
         }
 
         if {$changed} {
-            writeCell $outChan $shipCode $dateIndex $lonCell $latCell
+            writeCell $outChan $dateIndex $lonCell $latCell
 
-            set currentShipCode $shipCode
             set currentDateIndex $dateIndex
             set currentLonCell $lonCell
             set currentLatCell $latCell
-
         }
     }
+
 }
 
 # Write the last cell data
-writeCell $outChan $shipCode $dateIndex $lonCell $latCell
+writeCell $outChan $dateIndex $lonCell $latCell
 
-close $inChan
 close $outChan
-puts ""
-
